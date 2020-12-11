d0cf00720420f0736cd9541d2887bf9478feacad
weilun.tsai <weilun.tsai0116@gmail.com>
2020-12-11 16:53:01 +0800

    delete: bug fix

--- application.py
+++ application.py
@@ -460,12 +460,12 @@ def read_dash(readit_n_clicks,
      State('signal_id-state', 'value'),
      State('description-state', 'value')])
 def delete_dash(delete_n_clicks, user_id, signal_id, signal_description):
-
     if (user_id == "" or signal_id == "") and delete_n_clicks is not None:
         delete = u'''Delete result: Fail! Lack of User ID or Signal ID'''
     elif not isExist(user_id, signal_id, mydb, mycursor) and delete_n_clicks is not None:  # todo: as mentioned in create_dash
         delete = u'''Delete result: Fail! (User ID, Signal ID) is not exist'''
     elif delete_n_clicks is not None:
+        deleteFrom_table(user_id, signal_id, mydb, mycursor)
         delete = u'''Delete result: Pass!'''
     else:
         delete = 'Delete: 0 times'
